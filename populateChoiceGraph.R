18bcd30dc9bd4768feffaacaccd4d5bc20be5c61
Brian Loyal <bloyal@gmail.com>
2015-01-02 17:22:08 -0600

    Add option objects to graph - w/o constraints

--- populateChoiceGraph.R
+++ populateChoiceGraph.R
@@ -13,6 +13,11 @@ getTestOptionObject <- function(){
       labels = "toys,plastic",
       keywords = "fire, rescue, emergency, plastic, wheels, red",
       #optional elements
+      date = "1/2/2015",
+      imageSrc = "http://ecx.images-amazon.com/images/I/61tDtJTMemL._SY355_.jpg",
+      locationName = "Gateway Arch",
+      locationAddress = "11 North 4th Street, St. Louis, MO 63102",
+      locationGPS = "38.624691,-90.184776",
       measures = list (
         "price" = list (
           "value"=11.95,
@@ -63,25 +68,36 @@ getTestOptionObject <- function(){
 
 createOptions <- function(graph, optionObject, transactionMax = 1000){
 
-  optionCount <- length(optionObject);
   optionObject <- updateOptionLabels(optionObject); #Update option list to match syntax
   optionObject <- updateKeywords(optionObject); #Add name and description words to keywords
   t <- newTransaction(graph);
   transactionCounter <- 0
-  for (i in 1:optionCount){
+  for (i in 1:length(optionObject)){
     transactionCounter <- transactionCounter + 1;
-    query <- paste("CREATE (o:",optionObject[[i]]$labels, " {
+    query <- paste("MATCH (o:Option) 
+              WITH count(o) as max_option_id
+              CREATE (o:",optionObject[[i]]$labels, " {
+              optionId: max_option_id + 1,
               name:{name}, 
               description:{description},
               provider:{provider},
-              keywords:{keywords}
+              keywords:{keywords},
+              date:{date},
+              imageSrc:{imageSrc},
+              locationName:{locationName},
+              locationAddress:{locationAddress},
+              locationGPS:{locationGPS}
               })", sep="");
-    print(query);
     appendCypher(t, query, 
                   name = optionObject[[i]]$name, 
                   description = optionObject[[i]]$description,
                   provider = optionObject[[i]]$provider,
-                  keywords = optionObject[[i]]$keywords
+                  keywords = optionObject[[i]]$keywords,
+                  date = optionObject[[i]]$date,
+                  imageSrc = optionObject[[i]]$imageSrc,
+                  locationName = optionObject[[i]]$locationName,
+                  locationAddress = optionObject[[i]]$locationAddress,
+                  locationGPS = optionObject[[i]]$locationGPS
                   );
     if (transactionCounter == transactionMax) {
       commit(t);
@@ -93,17 +109,41 @@ createOptions <- function(graph, optionObject, transactionMax = 1000){
   
 }
 
-updateOptionLabels <- function(optionlist){
-  lapply(optionlist, function(x) {x$labels <- combineAllLabels(x$labels); return(x)});
+updateOptionLabels <- function(optionList){
+  lapply(optionList, function(x) {x$labels <- combineAllLabels(x$labels); return(x)});
 }
 
 combineAllLabels <- function(labelStr, additional = "Option"){
   splitString <- c(additional, unlist(strsplit(labelStr, ",")));
   splitString <- gsub("\\s","", splitString); 
-  splitString <- capitalize(splitString);
-  paste(splitString, collapse=":");
+  paste(toupper(substring(splitString, 1,1)), substring(splitString, 2),
+        sep="", collapse=":")
 }
 
 updateKeywords <- function(optionList){
-  return(optionList);
+  lapply(optionList, function(x) {x$keywords <- consolidateKeywords(x$name, x$description, x$keywords); return(x)});
+}
+
+consolidateKeywords<- function(name, description, keywords) {
+  library(tm);
+  library(reshape);
+  library(stringr);
+  
+  #combine names, descriptions, and user-defined keywords
+  keywords<-paste(name, description, keywords);
+  
+  #Process text with tm
+  corp <- VCorpus(VectorSource(keywords))
+  corp <- tm_map(corp, content_transformer(tolower))
+  corp <- tm_map(corp, removePunctuation)
+  corp <- tm_map(corp, removeWords, stopwords("english"))
+  corp <- tm_map(corp, stripWhitespace)
+  corp <- tm_map(corp, stemDocument)
+  keywords<-as.character(unlist(sapply(corp, `[`, "content")), stringsAsFactors=F)
+  
+  #Tokenize
+  keywords<-lapply(keywords,scan_tokenizer);
+  keywords<-lapply(keywords,unique);
+  keywords<- paste(unlist(keywords), collapse=",");
+  
 }
